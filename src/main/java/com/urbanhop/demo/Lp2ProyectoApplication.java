bf2e7ab7785dd6394a779b87de951b4c2cb6027b
drbh2020 <berastaindiego@gmail.com>
2024-06-20 21:12:20 -0500

    feat - inicio de proyecto spring

---
+++ src/main/java/com/urbanhop/demo/Lp2ProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.urbanhop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2ProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2ProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbanhop/demo/Lp2ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanhop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
